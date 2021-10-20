4a8d11fdfdafd3720b56ee4d700e5496cee8760a
Sireeshamakireddi <92502053+Sireeshamakireddi@users.noreply.github.com>
2021-10-20 13:39:38 +0530

    Add files via upload

---
+++ Class1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace classImplementation_Day1
+{
+   partial class LMS:Asessment
+    {
+        string name;
+        int mobileNo;
+        string email;
+        public void Display()
+            void Asessment.MQS_Assesement();
+        {
+            System.Console.WriteLine("Implementing void");
+        }
+        
+    }
+     partial class LMS =  Assesment,placement
+    {
+        public void Messege()
+        { 
+            Console.WriteLine(" This messege is coming from Second class");
+        }
+    }
+    abstract class placement
+    {
+        string Name_Of_company;
+         int No_of_student_placed;
+        DateTime year_Of_placement;
+        public void placement_record()
+    {
+        Console.WriteLine("This method will show you complete  record of the placement");
+    }
+    
+    }
+ public interface Asessment
+{
+    public void MCQ_Assesment();
+    public void QuestionBank();
+}
+ 
+
---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace classImplementation_Day1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           LMS Akash = new LMS();
+            Akash.Display();
+            Akash.Messege();
+            Akash.Placement_record();
+            Asessment Delhi;
+            Delhi.MCQ_Assesment();
+            Delhi.QuestionBank();
+
+           // placement NIT = new placement();
+        }
+    }
+}
